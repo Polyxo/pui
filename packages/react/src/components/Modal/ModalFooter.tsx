73167f8ddeb0d068e3ef751aabe7c64684efd60f
cassandra <cassandra.kesewa@gmail.com>
2024-02-21 08:59:54 +0000

    restructure nav ites and sidebar items

--- packages/react/src/components/Modal/ModalFooter.tsx
+++ packages/react/src/components/Modal/ModalFooter.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import Button from '../Button';
-import useSettings from '../../hooks/useSettings';
-import { ModalProps } from './Modal';
+import * as React from "react";
+import Button from "../Button";
+import useSettings from "../../hooks/useSettings";
+import { ModalProps } from "./Modal";
 
 export interface ModalFooterProps extends ModalProps {
   /*passiveModal?: boolean;
   secondaryButtonText?: string; */
   onSecondaryButtonClick?: (
     evt: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-    trigger: 'button' | 'background' | 'key'
+    trigger: "button" | "background" | "key"
   ) => void;
   prefix?: string;
 }
@@ -33,22 +33,24 @@ const ModalFooter: React.FC<ModalFooterProps> = ({
       <div className={`${prefix}--modal__buttons-container`}>
         {secondaryButtonText && (
           <Button
-            kind={danger ? 'tertiary' : 'secondary'}
+            kind="secondary"
             disabled={secondaryButtonDisabled}
             id="secondaryButton"
             onClick={(e) => {
-              if (onSecondaryButtonClick) onSecondaryButtonClick(e, 'button');
+              if (onSecondaryButtonClick) onSecondaryButtonClick(e, "button");
             }}
-            ref={secondaryButtonRef as React.Ref<HTMLButtonElement>}>
+            ref={secondaryButtonRef as React.Ref<HTMLButtonElement>}
+          >
             {secondaryButtonText}
           </Button>
         )}
         <Button
-          kind={danger ? 'danger--primary' : 'primary'}
+          kind={danger ? "danger" : "primary"}
           disabled={primaryButtonDisabled}
           onClick={onRequestSubmit}
           id="primaryButton"
-          ref={primaryButtonRef as React.Ref<HTMLButtonElement>}>
+          ref={primaryButtonRef as React.Ref<HTMLButtonElement>}
+        >
           {primaryButtonText}
         </Button>
       </div>
